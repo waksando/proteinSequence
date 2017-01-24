fad8b735412f74bbb38f42d542f9493df201af43
waksando <waksando@users.noreply.github.com>
2017-01-24 10:03:01 +0400

    FileManipulation

--- FileM.java
+++ FileM.java
@@ -8,23 +8,32 @@ import java.util.LinkedList;
 import java.util.regex.*;
 import java.util.regex.Matcher;
 import java.lang.*;
+import java.util.Scanner.*;
 
 
 
-public class FileM {
-    private static final String FILENAME = "C:\\Users\\sandooyea\\yr2\\lab\\src\\uniprot_sprot.fasta";
-    private static LinkedList<String> sequence=new LinkedList<String>();
-    private static boolean firstRead=false;
+public class FileManipulation {
+    private String FILENAME;
+    private LinkedList<String> sequence=new LinkedList<String>();
+    private LinkedList<String> id=new LinkedList<String>();
+    private LinkedList<String> os=new LinkedList<String>();
 
-    public static void main(String[] args) {
+
+    private boolean firstRead=false;
+
+
+    public void setFilename(String Filename){
+        this.FILENAME=Filename;
+    }
+    public void ReadFile() {
 
         final long startTime = System.nanoTime();
 
         BufferedReader br = null;
         FileReader fr = null;
-        String RegEx="^sp|([A-Z][A-Z|0-9]{4}[0-9])\\|";
-        String stext="OS";
-        String stext2="GN";
+        String stext="OS=";
+        String stext2="GN=";
+        String stext3="PE=";
 
 
         try {
@@ -32,44 +41,43 @@ public class FileM {
             fr = new FileReader(FILENAME);
             br = new BufferedReader(fr);
 
-            Pattern p1 = Pattern.compile(RegEx);
             Pattern p2 = Pattern.compile(stext);
             Pattern p3 = Pattern.compile(stext2);
+            Pattern p4 = Pattern.compile(stext3);
             String sCurrentLine;
 
             long count=0;
-            int test=0;
             int start,end;
             String str="";
             while ((sCurrentLine = br.readLine()) != null) {
-                Matcher matcher = p1.matcher(sCurrentLine);
                 Matcher matcher2 = p2.matcher(sCurrentLine);
                 Matcher matcher3 = p3.matcher(sCurrentLine);
+                Matcher matcher4 = p4.matcher(sCurrentLine);
                 //System.out.println(sCurrentLine);
-                test++;
-                if (test==30){
-                    break;
-                }
-                //id extract
-/*                while(matcher.find()) {
-                    *//*System.out.println("Group: " + matcher.group(1) + " : "
-                            + matcher.start() + " - " + matcher.end());*//*
-                    count++;
-                }*/
-                //os extract
-/*                while ((matcher2.find())&&(matcher3.find())){
-                    System.out.println("OS: "+matcher2.start()+" GN: "+matcher3.start());
-                    start=matcher2.start()+3;
-                    end=matcher3.start()-1;
-                    if (start<end) {
-                        System.out.println(sCurrentLine.substring(start, end));
-                    }
-                    else {
-                        end=sCurrentLine.length()-1;
-                        System.out.println(sCurrentLine.substring(start, end));
 
+                //id extract
+                if (sCurrentLine.charAt(0)=='>'){
+                    count++;
+                    id.add(sCurrentLine.substring(4,10));
+                }
+
+                //os extract
+                if ((sCurrentLine.charAt(0) == '>')&& (matcher2.find())) {
+                    start=matcher2.start()+3;
+                    if (matcher3.find()) {
+                        end=matcher3.start()-1;
+                        os.add(sCurrentLine.substring(start, end));
+/*                        System.out.println(matcher2.start() + " --ooo--" + matcher2.end());
+                        System.out.println(matcher3.start() + " ggg]]]]" + matcher3.end());*/
                     }
-                }*/
+                    else if (matcher4.find()) {
+                        end=matcher4.start()-1;
+                        os.add(sCurrentLine.substring(start, end));
+/*                        System.out.println(matcher2.start() + "ooooo " + matcher2.end());
+                        System.out.println(matcher4.start() + "ppppp " + matcher4.end());*/
+                    }
+                }
+
                 //reading sequence
                 if (sCurrentLine.charAt(0)!='>'){
                     firstRead=true;
@@ -81,9 +89,10 @@ public class FileM {
                 }
             }
             System.out.println(count);
-            for (String x: sequence){
-                System.out.println(x);
-            }
+            System.out.println("ID: "+id.size()+" OS: "+os.size()+" Sequence:\n"+sequence.size());
+/*            for (int i=0;i<sequence.size();i++){
+                System.out.println("ID: "+id.get(i)+" OS: "+os.get(i)+" Sequence:"+sequence.get(i));
+            }*/
 
         } catch (IOException e) {
             System.out.println("Error");
